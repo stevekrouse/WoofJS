f5a1d192a4a9b6bf64263d01247699fbb4c877b4
Steve Krouse <steveykrouse@gmail.com>
2016-05-12 17:10:11 -0400

    removed Number.prototype.between

--- woof.es6.js
+++ woof.es6.js
@@ -4,12 +4,6 @@ var Project = () => {
 
 var Woof = {};
 
-Number.prototype.between = function(a, b, inclusive) {
-  var min = Math.min.apply(Math, [a, b]),
-    max = Math.max.apply(Math, [a, b]);
-  return inclusive ? this >= min && this <= max : this > min && this < max;
-};
-
 Woof.keyCodeToString = (keyCode) => {
   if (keyCode == 38) {
     return "UP";
